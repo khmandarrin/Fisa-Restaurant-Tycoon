6637e10a2d3bcbebb8d97ceedf7589aeddc2687a
khmandarrin <jeong-ga-eun@jeong-ga-eun-ui-MacBookAir.local>
2026-02-12 12:17:29 +0900

    kitchen

--- src/main/java/core/Kitchen.java
+++ src/main/java/core/Kitchen.java
@@ -1,5 +1,51 @@
 package core;
 
-public class Kitchen {
+import java.util.ArrayList;
+import java.util.List;
 
+import thread.ChefWorker;
+
+public class Kitchen {
+    private int chefCount;
+    private final List<ChefWorker> chefs = new ArrayList<>();
+    private final List<Thread> chiefThreads = new ArrayList<>();
+    private QueueManager queueManager;
+
+    public void start(int chefCount, QueueManager queueManager) {
+        this.chefCount = chefCount;
+        this.queueManager = queueManager;
+
+        // 요리사 투입
+        for (int i = 0; i < chefCount; i++) {
+            ChefWorker chief = new ChefWorker(i, queueManager);
+            Thread thread = new Thread(chief, "요리사#" + i);
+            thread.start();
+            
+            chefs.add(chief);
+            chiefThreads.add(thread);
+        }
+        
+        System.out.println("[주방] 요리사 " + chefCount + "명 투입 완료");
+    }
+
+    public List<String> getStatusReport() {
+        List<String> report = new ArrayList<>();
+        
+        for (ChefWorker chef : chefs) {
+            report.add(chef.getStatusString());
+        }
+        
+        return report;
+    }
+
+    public void stop() {
+        for (ChefWorker chief : chefs) {
+        	chief.stop();
+        }
+        System.out.println("[주방] 영업 종료");
+    }
+
+    public List<ChefWorker> getChiefs() {
+        return chefs;
+    }
 }
